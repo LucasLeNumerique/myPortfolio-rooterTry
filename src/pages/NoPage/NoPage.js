789eac4a67ea60e6102996021587e4cd7034c411
ekypow <lucas.schrever@gmail.com>
2022-02-06 23:50:04 +0100

    end of 404

--- src/pages/NoPage/NoPage.js
+++ src/pages/NoPage/NoPage.js
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+import './NoPage.sass'
+
 function NoPage() {
     return (
-        <h1>404 | Page inconnue</h1>
+        <>
+            <h1> Oups ! Il semblerait que vous ayez inséré un lien URL inexistant ! </h1>
+            <h2> Pour les habitués, c'est une erreur 404 </h2>
+
+            <p> Laissez-moi vous raccompagner vers le <Link className="from-404-to-home" to='/' exact="true">Menu</Link> !
+
+            </p>
+        </>
     )
 }
 
